b5e1c399ae73eab8ee62cd6fd856939301010c5f
pak-laura <laurapak2@gmail.com>
2017-11-16 20:52:20 -0600

    Update mult.v

--- mult.v
+++ mult.v
@@ -20,10 +20,10 @@ module Mult(first, second, errorWire, outWire);
 	FAdder #(8) fa1(pp1, {1'b0,pp0[7:1]}, 1'b0, addOut1, sum1);
 	FAdder #(8) fa2(pp2, {addOut1,sum1[7:1]}, 1'b0, addOut2, sum2);
 	FAdder #(8) fa3(pp3, {addOut2,sum2[7:1]}, 1'b0, addOut3, sum3);
-	FAdder #(8) fa2(pp4, {addOut3,sum3[7:1]}, 1'b0, addOut4, sum4);
-	FAdder #(8) fa2(pp5, {addOut4,sum4[7:1]}, 1'b0, addOut5, sum5);
-	FAdder #(8) fa2(pp6, {addOut5,sum5[7:1]}, 1'b0, addOut6, sum6);
-	FAdder #(8) fa2(pp7, {addOut6,sum6[7:1]}, 1'b0, addOut7, sum7);
+	FAdder #(8) fa4(pp4, {addOut3,sum3[7:1]}, 1'b0, addOut4, sum4);
+	FAdder #(8) fa5(pp5, {addOut4,sum4[7:1]}, 1'b0, addOut5, sum5);
+	FAdder #(8) fa6(pp6, {addOut5,sum5[7:1]}, 1'b0, addOut6, sum6);
+	FAdder #(8) fa7(pp7, {addOut6,sum6[7:1]}, 1'b0, addOut7, sum7);
 
 	//result
 	wire[7:0] outWire = {sum7[0], sum6[0], sum5[0], sum4[0], sum3[0], sum2[0],sum1[0], pp0[0]};
