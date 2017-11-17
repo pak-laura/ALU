d121ffa45bbd09f890651ee1080d3211042f852a
pak-laura <laurapak2@gmail.com>
2017-11-17 13:58:56 -0600

    Update main_tb.v

--- main_tb.v
+++ main_tb.v
@@ -17,8 +17,9 @@ module TestBench;
 		forever
 			begin
 				#5;
-				$display("    %b (%d)|    %b (%d)|   %b|   %b|   %b (%d)|  %b",final1, final1, final2, final2, out_sel, currState, out, out, nextState);
-				$display("    %b (%d)|    %b (%d)|   %b|   %b|   %b (%d)|  %b",final1, final1, final2, final2, out_sel, currState, out, out, nextState);
+				$display("    %b (%d)|    %b (%d)|   %b|",final1, final1, final2, final2, out_sel);
+				#5;
+				$display("    %b|   %b (%d)|  %b", currState, out, out, nextState);
 				$display("");
 				#5 clk = 1;
 				#5 clk = 0;
