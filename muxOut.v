8b232af685afc36cf4abd95a1444f51ab9ae73f8
pak-laura <laurapak2@gmail.com>
2017-11-16 21:04:55 -0600

    Update muxOut.v

--- muxOut.v
+++ muxOut.v
@@ -2,7 +2,7 @@ module MuxOut(logAnd, logOr, logXor, logNot, add, sub, mult, sel, outWire);
 	parameter k =7; //k = # inputs. can override in instantiation
 	//override when called like MuxOut #(k_override_num) Instance_of_MuxOut(logAdd_thing, logOr_thing,...);
 	input [k-1:0] logAnd, logOr, logXor, logNot, add, sub, mult; //inputs
-	input [5:0] sel; //1-hot select
+	input [k-1:0] sel; //1-hot select
 	output[k-1:0] outWire;
 	wire [k-1:0] outWire = ({k{sel[0]}} & logAnd) |
 				({k{sel[1]}} & logOr) |
