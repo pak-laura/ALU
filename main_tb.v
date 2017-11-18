b824bd7bc60c31e69d4f3a319c9519af570cad7c
pak-laura <laurapak2@gmail.com>
2017-11-17 18:37:14 -0600

    Update main_tb.v

--- main_tb.v
+++ main_tb.v
@@ -27,7 +27,7 @@ module TestBench;
 			#20;
 			/*rst=0;*/ on = 1'b1; in_sel = 3'b010; num1 = 8'b01010111; num2 = 8'b00011010; out_sel = 7'b1000000;
 			#20;
-			/*rst =1;*/ on = 1'b1; in_sel = 3'b001; num1 = 8'b01010111; num2 = 8'b00011010; out_sel = 7'b1000000;
+			/*rst =1;*/ on = 1'b1; in_sel = 3'b100; num1 = 8'b01010111; num2 = 8'b00011010; out_sel = 7'b1000000;
 			#20;
 			#20;
 			#20;
