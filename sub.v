292eaa4bb970411cf6558a173f7c1eeeccbf95d9
Fariha97 <fni150030@utdallas.edu>
2017-11-20 19:51:35 -0600

    Update sub.v

--- sub.v
+++ sub.v
@@ -1,17 +1,6 @@
-module sub(first, second, diff, error);
+module sub(first, second, diff);
 	input[7:0] first, second;
 	output [7:0] diff;
-	output error;
-	reg x;
 
 	assign{diff} = first-second;
-	always @(*) begin
-		if (second > first)
-			x=1;
-		else
-			x=0;
-	end
-
-	assign error = x;
-
 endmodule
